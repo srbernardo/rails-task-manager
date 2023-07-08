eaf55fd48b8436f71e43da1f9238d11e7682ac60
bernardo brotelli <b.brotelle@gmail.com>
2023-07-08 13:46:19 -0300

    add migrate change add new colunm

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_164026) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_164306) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
